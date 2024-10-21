30b25dd8c848736abb48c4c2a2b0537f9505bcc8
sebgrgs <sebgrgs@ubuntu.myguest.virtualbox.org>
2024-10-21 12:14:38 +0200

    Write a C program that prints exactly words followed by a new line with puts

---
+++ hello_world/4-puts.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - function where the program starts
+ *
+ * Return: always 0 success
+ */
+
+int main(void)
+{
+	puts("\"Programming is like a multilingual puzzle");
+	return (0);
+}
