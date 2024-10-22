f4b8b34808ec43af1d02a5f3cd86a8a3f06d9209
sebgrgs <sebastien.georges11@gmail.com>
2024-10-22 09:47:59 +0200

    print the last digit of a number and use of if else and else if

---
+++ variables_if_else_while/1-last_digit.c
@@ -0,0 +1,26 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <time.h>
+
+/**
+* main - function where the program starts
+* Return: always 0 success
+*/
+
+int main(void)
+{
+	int n;
+	int lastDigit;
+
+srand(time(0));
+n = rand() - RAND_MAX / 2;
+lastDigit = n % 10;
+
+if (lastDigit > 5)
+	printf("Last digit of %d is %d and is greather than 5\n", n, lastDigit);
+else if (lastDigit == 0)
+	printf("Last digit of %d is %d and is 0\n", n, lastDigit);
+else
+	printf("Last digit of %d is %d and is less than 6 and not 0\n", n, lastDigit);
+return (0);
+}
