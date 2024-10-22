b49ee729990ffba28d68340a47eceaade90cae80
sebgrgs <sebastien.georges11@gmail.com>
2024-10-22 09:54:49 +0200

    print the last digit of a number and use of if else and else if

--- variables_if_else_while/1-last_digit.c
+++ variables_if_else_while/1-last_digit.c
@@ -17,7 +17,7 @@ n = rand() - RAND_MAX / 2;
 lastDigit = n % 10;
 
 if (lastDigit > 5)
-	printf("Last digit of %d is %d and is greather than 5\n", n, lastDigit);
+	printf("Last digit of %d is %d and is greater than 5\n", n, lastDigit);
 else if (lastDigit == 0)
 	printf("Last digit of %d is %d and is 0\n", n, lastDigit);
 else
