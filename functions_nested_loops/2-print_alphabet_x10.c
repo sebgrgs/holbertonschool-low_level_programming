3f0e31b0da67d6d5952b68801423b635b0f7525e
sebgrgs <sebastien.georges11@gmail.com>
2024-10-23 15:39:28 +0200

    write a function thar prints alphabet lower 10 times with another function and while

---
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - function that print alphabet lower 10 times
+ * Return: 0 always success
+ */
+
+void print_alphabet_x10(void)
+{
+	int n = 0;
+
+	while (n < 9)
+
+	{
+		print_alphabet();
+		n++;
+	}
+
+
+}
