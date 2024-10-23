bb641c0270e335895bb95ed9552ca2d18120842a
sebgrgs <sebastien.georges11@gmail.com>
2024-10-23 15:49:40 +0200

    write a function thar prints alphabet lower 10 times with another function and while

--- functions_nested_loops/2-print_alphabet_x10.c
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -1,6 +1,24 @@
 #include "main.h"
 
 /**
+ * print_alphabet - function that print alphabet lower
+ * Return: 0 always success
+ */
+
+void print_alphabet(void)
+{
+	char c;
+
+	for (c = 'a'; c <= 'z'; c++)
+{
+	_putchar(c);
+}
+_putchar('\n');
+
+}
+
+
+/**
  * print_alphabet_x10 - function that print alphabet lower 10 times
  * Return: 0 always success
  */
