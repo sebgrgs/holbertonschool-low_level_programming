fc44fa6645e2e2504437760f701731a3476309f5
sebgrgs <sebastien.georges11@gmail.com>
2024-10-23 13:55:12 +0200

    write a function that prints the alphabet lowercase with putchar and print_alphabet

---
+++ functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * print_alphabet - function that print alphabet lower
+ * Return: 0 always success
+ */
+
+void print_alphabet(void)
+{
+	char c;
+
+	for (c = 'a'; c <= 'z'; c++)
+{
+	_putchar(c);
+}
+_putchar('\n');
+
+}
