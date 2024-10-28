db2452e2376b1415d4261647d67370df4c600df5
sebgrgs <sebastien.georges11@gmail.com>
2024-10-28 10:13:02 +0100

    functions that prints digits

---
+++ more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * print_numbers - functions that prints digits from 0 to 9
+ */
+
+void print_numbers(void)
+{
+int n;
+	for (n = 0; n < 10; n++)
+	{
+		_putchar (n + '0');
+	}
+_putchar('\n');
+}
