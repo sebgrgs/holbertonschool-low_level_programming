92ad2c93f0f1da84bfd91c1e0dc730eabcad5f79
sebgrgs <sebastien.georges11@gmail.com>
2024-10-30 13:27:57 +0100

    function that convert a string to an integer

---
+++ pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,45 @@
+#include "main.h"
+
+/**
+ * _atoi - function that converts string into integer
+ * @s: the pointer to the string
+ * Return: Result
+ */
+
+int _atoi(char *s)
+{
+	int result = 0;
+	int i = 0;
+	int signe = 1;
+	int started = 0;
+
+	while (s[i] == ' ')
+	{
+		i++;
+	}
+
+	if (s[i] == '-')
+	{
+		signe = -1;
+		i++;
+	}
+	else if (s[i] == '+')
+	{
+		i++;
+	}
+
+	while (s[i] != '\0')
+	{
+		if (s[i] >= '0' && s[i] <= '9')
+		{
+			result = result * 10 + (s[i] - '0');
+			started = 1;
+		}
+		else if (started)
+		{
+			break;
+		}
+		i++;
+	}
+	return (signe * result);
+}
