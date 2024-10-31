47f83dec5cbe0b7a2e84f2efd8c21ede962ba1d7
sebgrgs <sebastien.georges11@gmail.com>
2024-10-31 21:29:33 +0100

    function that copies a string

--- pointers_arrays_strings/2-strncpy.c
+++ pointers_arrays_strings/2-strncpy.c
@@ -1,8 +1,10 @@
 #include "main.h"
-#include <stdio.h>
 /**
- * 
- * 
+ * _strncpy - function that copies a string
+ * @dest: destination string
+ * @src: source string
+ * @n: number of character to copies
+ * Return: the destination string
  */
 
 char *_strncpy(char *dest, char *src, int n)
@@ -22,37 +24,4 @@ char *_strncpy(char *dest, char *src, int n)
 dest[i] = '\0';
 
 	return (dest);
-}
-int main(void)
-{
-    char s1[98];
-    char *ptr;
-    int i;
-
-    for (i = 0; i < 98 - 1; i++)
-    {
-        s1[i] = '*';
-    }
-    s1[i] = '\0';
-    printf("%s\n", s1);
-    ptr = _strncpy(s1, "First, solve the problem. Then, write the code\n", 5);
-    printf("%s\n", s1);
-    printf("%s\n", ptr);
-    ptr = _strncpy(s1, "First, solve the problem. Then, write the code\n", 90);
-    printf("%s", s1);
-    printf("%s", ptr);
-    for (i = 0; i < 98; i++)
-    {
-        if (i % 10)
-        {
-            printf(" ");
-        }
-        if (!(i % 10) && i)
-        {
-            printf("\n");
-        }
-        printf("0x%02x", s1[i]);
-    }
-    printf("\n");
-    return (0);
 }
