ddd91b4a7b34880105478a3cafb26a26b84882e4
sebgrgs <sebastien.georges11@gmail.com>
2024-11-14 21:04:03 +0100

    function that creates a new dog

--- structures_typedef/4-new_dog.c
+++ structures_typedef/4-new_dog.c
@@ -14,19 +14,19 @@ dog_t *new_dog(char *name, float age, char *owner)
 {
 	dog_t *dog;
 	char *name_copy, *owner_copy;
-	int name_len = 0, owner_len = 0, i = 0;
+	int name_len = 0, owner_len = 0, i;
 
 	if (name == NULL || owner == NULL)
 		return (NULL);
 
-	while (name[name_len])
-		name_len++;
-	while (owner[owner_len])
-		owner_len++;
-
 	dog = malloc(sizeof(dog_t));
 	if (dog == NULL)
 		return (NULL);
+
+	for (i = 0; name[i] != '\0'; i++)
+		name_len++;
+	for (i = 0; owner[i] != '\0'; i++)
+		owner_len++;
 
 	name_copy = malloc(name_len + 1);
 	if (name_copy == NULL)
@@ -34,9 +34,6 @@ dog_t *new_dog(char *name, float age, char *owner)
 		free(dog);
 		return (NULL);
 	}
-	for (; i <= name_len; i++)
-		name_copy[i] = name[i];
-
 	owner_copy = malloc(owner_len + 1);
 	if (owner_copy == NULL)
 	{
@@ -44,12 +41,15 @@ dog_t *new_dog(char *name, float age, char *owner)
 		free(dog);
 		return (NULL);
 	}
-	for (; i <= owner_len; i++)
+	for (i = 0; name[i] != '\0'; i++)
+		name_copy[i] = name[i];
+	name_copy[name_len] = '\0';
+	for (i = 0; owner[i] != '\0'; i++)
 		owner_copy[i] = owner[i];
+	owner_copy[owner_len] = '\0';
 
 	dog->name = name_copy;
 	dog->age = age;
 	dog->owner = owner_copy;
-
 	return (dog);
 }
