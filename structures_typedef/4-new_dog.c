935d7bdb53b7e9e9300248b6717f74ffa16ebdd1
sebgrgs <sebastien.georges11@gmail.com>
2024-11-14 20:44:00 +0100

    function that creates a new dog

--- structures_typedef/4-new_dog.c
+++ structures_typedef/4-new_dog.c
@@ -16,6 +16,9 @@ dog_t *new_dog(char *name, float age, char *owner)
 	char *name_copy, *owner_copy;
 	int name_len = 0, owner_len = 0, i = 0;
 
+	if (name == NULL || owner == NULL)
+		return (NULL);
+
 	while (name[name_len])
 		name_len++;
 	while (owner[owner_len])
@@ -31,7 +34,6 @@ dog_t *new_dog(char *name, float age, char *owner)
 		free(dog);
 		return (NULL);
 	}
-
 	for (; i <= name_len; i++)
 		name_copy[i] = name[i];
 
