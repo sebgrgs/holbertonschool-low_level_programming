10778623058d1d116e4bcbb6a87b015ba782a457
sebgrgs <sebastien.georges11@gmail.com>
2024-11-14 15:58:34 +0100

    update header typedef

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -8,7 +8,7 @@
  * @owner: owner of the dog
  */
 
-struct dog
+typedef struct dog
 {
 	char *name;
 
@@ -16,7 +16,7 @@ struct dog
 
 	char *owner;
 
-};
+} dog;
 
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
