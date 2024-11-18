92b89e5341dfdb50bcd7becbaab91ace07c81d06
sebgrgs <sebastien.georges11@gmail.com>
2024-11-18 11:54:27 +0100

    function that prints a name

---
+++ function_pointers/0-print_name.c
@@ -0,0 +1,16 @@
+#include "function_pointers.h"
+#include <stdio.h>
+
+/**
+ * print_name - function that prints a name
+ * @name: string for name of the person
+ * @f: pointer to a function that prints a name
+ */
+
+void print_name(char *name, void (*f)(char *))
+{
+	if (name != NULL && f != NULL)
+	{
+		f(name);
+	}
+}
