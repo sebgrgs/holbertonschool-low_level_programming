425f283f6fafa374a4f2bb992241abcb92270aa1
sebgrgs <sebastien.georges11@gmail.com>
2024-11-19 21:07:11 +0100

    function that prints a name

--- function_pointers/0-print_name.c
+++ function_pointers/0-print_name.c
@@ -5,6 +5,7 @@
  * print_name - function that prints a name
  * @name: string for name of the person
  * @f: pointer to a function that prints a name
+ * Return: void
  */
 
 void print_name(char *name, void (*f)(char *))
