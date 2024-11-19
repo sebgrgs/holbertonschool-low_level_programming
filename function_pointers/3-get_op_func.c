3c451843fbd431169f87a594caf56c735a6dafc8
sebgrgs <sebastien.georges11@gmail.com>
2024-11-19 10:16:59 +0100

    program that performs simple operations

---
+++ function_pointers/3-get_op_func.c
@@ -0,0 +1,36 @@
+#include "3-calc.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+/**
+ * get_op_func - selects the correct function to perform the operation
+ * @s: operator passed as argument to the program
+ * Return: pointer to the function that corresponds to
+ * the operator given as a parameter
+ * if s does not match any of the 5 expected operators, return NULL
+ */
+
+int (*get_op_func(char *s))(int, int)
+{
+	op_t ops[] = {
+		{"+", op_add},
+		{"-", op_sub},
+		{"*", op_mul},
+		{"/", op_div},
+		{"%", op_mod},
+		{NULL, NULL}
+	};
+	int i = 0;
+
+	while (ops[i].op != NULL)
+	{
+		if (*s == *ops[i].op && *(s + 1) == '\0')
+		{
+			return (ops[i].f);
+		}
+		i++;
+	}
+	return (NULL);
+
+}
---
+++ function_pointers/3-main.c
@@ -0,0 +1,37 @@
+#include "3-calc.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - performs simple operations
+ * @argc: number of arguments
+ * @argv: array of arguments
+ * Return: 0
+ */
+
+int main(int argc, char *argv[])
+{
+	int num1, num2, result;
+	int (*op)(int, int);
+
+	if (argc != 4)
+	{
+		printf("Error\n");
+		exit(98);
+	}
+
+	num1 = atoi(argv[1]);
+	num2 = atoi(argv[3]);
+	op = get_op_func(argv[2]);
+
+	if (op == NULL)
+	{
+		printf("Error\n");
+		exit(99);
+	}
+
+	result = op(num1, num2);
+	printf("%d\n", result);
+
+	return (0);
+}
