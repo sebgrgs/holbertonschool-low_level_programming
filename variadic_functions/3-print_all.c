cf38180e1176545a4b31ff7f657f9408a58ce0d3
sebgrgs <sebastien.georges11@gmail.com>
2024-11-20 22:02:38 +0100

    start of printf variadic

---
+++ variadic_functions/3-print_all.c
@@ -0,0 +1,12 @@
+#include "variadic_functions.h"
+#include <stdio.h>
+
+/**
+ * print_all - prints anything
+ * @format: list of types of arguments passed to the function
+ */
+
+void print_all(const char * const format, ...)
+{
+	
+}
