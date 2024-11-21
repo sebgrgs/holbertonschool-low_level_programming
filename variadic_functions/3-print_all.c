0193c5209c1a2bb7e623dd0e067bf87f32bf23a8
sebgrgs <sebastien.georges11@gmail.com>
2024-11-21 13:59:28 +0100

    function that prints anything

--- variadic_functions/3-print_all.c
+++ variadic_functions/3-print_all.c
@@ -8,5 +8,86 @@
 
 void print_all(const char * const format, ...)
 {
-	
+	va_list ap;
+	int i = 0;
+	int j;
+	char *separator = "";
+
+	print_t types[] = {
+		{"c", print_char},
+		{"i", print_int},
+		{"f", print_float},
+		{"s", print_string},
+		{NULL, NULL}
+	};
+
+	va_start(ap, format);
+
+	while (format && format[i])
+	{
+		j = 0;
+		while (types[j].type)
+		{
+			if (format[i] == types[j].type[0])
+			{
+				printf("%s", separator);
+				types[j].f(ap);
+				separator = ", ";
+				break;
+			}
+			j++;
+		}
+	i++;
+	}
+	printf("\n");
+	va_end(ap);
+}
+
+/**
+ * print_char - prints a char
+ * @ap: argument pointer
+ */
+
+void print_char(va_list ap)
+{
+	printf("%c", va_arg(ap, int));
+}
+
+/**
+ * print_int - prints an int
+ * @ap: argument pointer
+ */
+
+void print_int(va_list ap)
+{
+	printf("%i", va_arg(ap, int));
+}
+
+/**
+ * print_float - prints a float
+ * @ap: argument pointer
+ */
+
+void print_float(va_list ap)
+{
+	printf("%f", va_arg(ap, double));
+}
+
+/**
+ * print_string - prints a string
+ * @ap: argument pointer
+ */
+
+void print_string(va_list ap)
+{
+	char *str;
+
+	str = va_arg(ap, char *);
+
+	if (str == NULL)
+	{
+		printf("(nil)");
+		return;
+	}
+	printf("%s", str);
 }
--- variadic_functions/variadic_functions.h
+++ variadic_functions/variadic_functions.h
@@ -3,10 +3,27 @@
 
 #include <stdarg.h>
 
+/**
+ * struct print - Struct print
+ * @type: The type of argument
+ * @f: The function associated
+ */
+
+typedef struct print
+{
+	char *type;
+	void (*f)(va_list);
+} print_t;
+
+
 int _putchar(char c);
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
 void print_all(const char * const format, ...);
+void print_char(va_list ap);
+void print_int(va_list ap);
+void print_float(va_list ap);
+void print_string(va_list ap);
 
 #endif
