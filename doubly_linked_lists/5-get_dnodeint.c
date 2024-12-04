c188c43555653ce68d90e9166232d19211f392f6
sebgrgs <sebastien.georges11@gmail.com>
2024-12-04 11:57:25 +0100

    Add get_dnodeint_at_index function to retrieve a node at a specific index in a doubly linked list and update header file

---
+++ doubly_linked_lists/5-get_dnodeint.c
@@ -0,0 +1,43 @@
+#include "lists.h"
+
+/**
+ * 
+ * 
+ */
+
+dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
+{
+	unsigned int i = 0;
+
+	while (head != NULL)
+	{
+		if (i == index)
+		{
+			return (head);
+		}
+		i++;
+		head = head->next;
+	}
+	return (NULL);
+}
+int main(void)
+{
+    dlistint_t *head;
+    dlistint_t *node;
+
+    head = NULL;
+    add_dnodeint_end(&head, 0);
+    add_dnodeint_end(&head, 1);
+    add_dnodeint_end(&head, 2);
+    add_dnodeint_end(&head, 3);
+    add_dnodeint_end(&head, 4);
+    add_dnodeint_end(&head, 98);
+    add_dnodeint_end(&head, 402);
+    add_dnodeint_end(&head, 1024);
+    print_dlistint(head);
+    node = get_dnodeint_at_index(head, 5);
+    printf("%d\n", node->n);
+    free_dlistint(head);
+    head = NULL;
+    return (EXIT_SUCCESS);
+}
