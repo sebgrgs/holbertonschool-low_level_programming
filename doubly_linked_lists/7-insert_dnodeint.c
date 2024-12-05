f76c8aaa6a300d2b50c7397b6129299a96dabad3
sebgrgs <sebastien.georges11@gmail.com>
2024-12-05 13:32:14 +0100

    Fix insert_dnodeint_at_index function to handle edge cases and improve node insertion logic

--- doubly_linked_lists/7-insert_dnodeint.c
+++ doubly_linked_lists/7-insert_dnodeint.c
@@ -13,27 +13,38 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	dlistint_t *new_node, *temp = *h;
 	unsigned int i = 0;
 
+	if (h == NULL)
+		return (NULL);
+
 	if (idx == 0)
 		return (add_dnodeint(h, n));
 
-	while (temp != NULL)
+	/* Traverse to position idx-1 */
+	while (temp != NULL && i < idx - 1)
 	{
-		if (i == idx - 1)
-		{
-			if (temp->next == NULL)
-				return (add_dnodeint_end(h, n));
-			new_node = malloc(sizeof(dlistint_t));
-			if (new_node == NULL)
-				return (NULL);
-			new_node->n = n;
-			new_node->next = temp->next;
-			new_node->prev = temp;
-			temp->next->prev = new_node;
-			temp->next = new_node;
-			return (new_node);
-		}
-		i++;
 		temp = temp->next;
+		i++;
 	}
-	return (NULL);
+
+	/* Check if we can insert at given position */
+	if (temp == NULL)
+		return (NULL);
+
+	/* If inserting at end */
+	if (temp->next == NULL)
+		return (add_dnodeint_end(h, n));
+
+	/* Create new node */
+	new_node = malloc(sizeof(dlistint_t));
+	if (new_node == NULL)
+		return (NULL);
+
+	/* Set up node connections */
+	new_node->n = n;
+	new_node->prev = temp;
+	new_node->next = temp->next;
+	temp->next->prev = new_node;
+	temp->next = new_node;
+
+	return (new_node);
 }
