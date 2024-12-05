1bba07dbfee77af3e934d4f341af213d596d94f6
sebgrgs <sebastien.georges11@gmail.com>
2024-12-05 09:27:23 +0100

    Update documentation for get_dnodeint_at_index function to clarify parameters and return value

--- doubly_linked_lists/5-get_dnodeint.c
+++ doubly_linked_lists/5-get_dnodeint.c
@@ -1,8 +1,10 @@
 #include "lists.h"
 
 /**
- * 
- * 
+ * get_dnodeint_at_index - returns the nth node of a dlistint_t linked list
+ * @head: pointer to the dlistint_t list
+ * @index: index of the node to return
+ * Return: the nth node of a dlistint_t linked list
  */
 
 dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
