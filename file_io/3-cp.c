cd37ef897a77d95012ffceefce938398dcce2507
sebgrgs <sebastien.georges11@gmail.com>
2024-12-06 15:24:14 +0100

    Improve error handling in file copy functionality and streamline main function

--- file_io/3-cp.c
+++ file_io/3-cp.c
@@ -24,18 +24,15 @@ void handle_close(int fd)
 int main(int ac, char *av[])
 {
 	int fd_from, fd_to;
-	ssize_t read_bytes;
+	ssize_t read_bytes, write_bytes;
 	char buffer[1024];
 
 	if (ac != 3)
-	{
-		dprintf(2, "Usage: cp file_from file_to\n");
-		exit(97);
+	{dprintf(2, "Usage: cp file_from file_to\n"), exit(97);
 	}
 	fd_from = open(av[1], O_RDONLY);
 	if (fd_from == -1)
-	{
-		dprintf(2, "Error: Can't read from file %s\n", av[1]);
+	{dprintf(2, "Error: Can't read from file %s\n", av[1]);
 		exit(98);
 	}
 	fd_to = open(av[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
@@ -46,13 +43,16 @@ int main(int ac, char *av[])
 		exit(99);
 	}
 	while ((read_bytes = read(fd_from, buffer, sizeof(buffer))) > 0)
-		if (write(fd_to, buffer, read_bytes) == -1)
+	{
+		write_bytes = write(fd_to, buffer, read_bytes);
+		if (write_bytes == -1 || write_bytes != read_bytes)
 		{
 			handle_close(fd_from);
 			handle_close(fd_to);
 			dprintf(2, "Error: Can't write to %s\n", av[2]);
 			exit(99);
 		}
+	}
 	if (read_bytes == -1)
 	{
 		handle_close(fd_from);
