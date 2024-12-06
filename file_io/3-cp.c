976bcf9703222fbfb69c9d6e5f658d245ec33fb8
sebgrgs <sebastien.georges11@gmail.com>
2024-12-06 19:23:07 +0100

    Update error handling in file copy functionality to use STDERR_FILENO for error messages

--- file_io/3-cp.c
+++ file_io/3-cp.c
@@ -9,7 +9,7 @@ void handle_close(int fd)
 {
 	if (close(fd) == -1)
 	{
-		dprintf(2, "Error: Can't close fd %d\n", fd);
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd);
 		exit(100);
 	}
 }
@@ -28,18 +28,18 @@ int main(int ac, char *av[])
 	char buffer[1024];
 
 	if (ac != 3)
-	{dprintf(2, "Usage: cp file_from file_to\n"), exit(97);
+	{dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n"), exit(97);
 	}
 	fd_from = open(av[1], O_RDONLY);
 	if (fd_from == -1)
-	{dprintf(2, "Error: Can't read from file %s\n", av[1]);
+	{dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", av[1]);
 		exit(98);
 	}
 	fd_to = open(av[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
 	if (fd_to == -1)
 	{
 		handle_close(fd_from);
-		dprintf(2, "Error: Can't write to %s\n", av[2]);
+		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", av[2]);
 		exit(99);
 	}
 	while ((read_bytes = read(fd_from, buffer, sizeof(buffer))) > 0)
@@ -49,7 +49,7 @@ int main(int ac, char *av[])
 		{
 			handle_close(fd_from);
 			handle_close(fd_to);
-			dprintf(2, "Error: Can't write to %s\n", av[2]);
+			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", av[2]);
 			exit(99);
 		}
 	}
@@ -57,7 +57,7 @@ int main(int ac, char *av[])
 	{
 		handle_close(fd_from);
 		handle_close(fd_to);
-		dprintf(2, "Error: Can't read from file %s\n", av[1]);
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", av[1]);
 		exit(98);
 	}
 	handle_close(fd_from);
