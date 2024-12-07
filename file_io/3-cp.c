cebc5239ba8075f8df0ccf72466311096488fbdf
sebgrgs <sebastien.georges11@gmail.com>
2024-12-07 14:41:45 +0100

    Refactor error handling in file copy functionality to ensure proper checks for file descriptors and streamline resource management

--- file_io/3-cp.c
+++ file_io/3-cp.c
@@ -36,19 +36,13 @@ int main(int ac, char *av[])
 		exit(98);
 	}
 	fd_to = open(av[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
-	if (fd_to == -1)
-	{
-		handle_close(fd_from);
-		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", av[2]);
-		exit(99);
-	}
+
 	while ((read_bytes = read(fd_from, buffer, sizeof(buffer))) > 0)
 	{
 		write_bytes = write(fd_to, buffer, read_bytes);
-		if (write_bytes == -1 || write_bytes != read_bytes)
+		if (fd_to == -1 || write_bytes != read_bytes)
 		{
 			handle_close(fd_from);
-			handle_close(fd_to);
 			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", av[2]);
 			exit(99);
 		}
@@ -56,7 +50,6 @@ int main(int ac, char *av[])
 	if (read_bytes == -1)
 	{
 		handle_close(fd_from);
-		handle_close(fd_to);
 		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", av[1]);
 		exit(98);
 	}
